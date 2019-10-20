bc357a6139f5b44574ddfc7b298f9511d53d9f59
Amit Bhalerao <amitbaramatimca@gmail.com>
2019-10-20 23:47:50 +0530

    Spring boot angular single deployable war

--- src/main/java/com/example/springbootpostgres/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootpostgres.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springbootpostgres.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
